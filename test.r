634f3e8dfaf6ac62970b9ff5fd7d3e079ad0d230
Alexander Pridava <sanyapridava@mail.ru>
2024-02-13 12:16:10 +0300

    ReDownload

--- test.r
+++ test.r
@@ -4,7 +4,8 @@ optimize_func <- function(x1, x2) {2 * x1 ** 2 + x1 * x2 + x2 ** 2}
 
 x <- -10:10
 y <- -10:10
+z = outer(x, y, optimize_func)
 
-#pmat <- persp3D(x = x, y = y, z)
-mypoints <- points3D(c(0, 10, 15), c(3, 4, 5), c(10, 15, 19), pmat=pmat)
+persp3D(x = x, y = y, z)
+#mypoints <- points3D(c(0, 10, 15), c(3, 4, 5), c(10, 15, 19), pmat=pmat)
 #points3D(mypoints, pch=16, col=2)
